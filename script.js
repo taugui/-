a738b36317f2342b982f8df0416e77d36c19c938
wtg378900 <56295698+wtg378900@users.noreply.github.com>
2020-05-19 12:07:20 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,10 @@
-// 设置正确的用户名和密码
-var correctUsername = "kido";
-var correctPassword = "shiwuxuetang";
+let guests = [];
+
+$(".addButton").click(function() {
+    let guest = $(".guestName").val();
+    guests.push(guest) ;
+	$(".numberOfGuests").text(guests.length);
+    $(".guestList").append(`<p>  ${guest} </p>`);
 
 
-$("button").click(function() {
-    var username = $(".username").val();
-    var password = $(".password").val();
-    
-    //在下面加入代码
-
-if( username === correctUsername && password ===correctPassword ){
-    alert("恭喜你登录成功！");
-    
-}else if(username ===""){
-	alert("亲！请输入用户名");
-    
-}else if(password === ""){
-    alert("亲！请输入密码");
-    
-}else if( username === correctUsername || password !==correctPassword ){
-	alert("Sorry!你的密码可能存在问题");
-    
-}else if( username !== correctUsername ||password===correctPassword ){
-	alert("Sorry!你的用户名可能存在问题");
-   
-}
-    
 });
-          
-function buttonClick(){  
-	alert("账号：kido  密码：shiwuxuetang");  
-}  
-
-
